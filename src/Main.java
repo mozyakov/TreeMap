77def9cd963222ef6fc2df822ecf98720c5632a4
mozyakov <s.mozyakov@gmail.com>
2022-10-21 16:58:22 +0700

    Main поменял порядок у двух строк в списке

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Person> person = new ArrayList<>(Arrays.asList(
-                new Person("Albert", "Van-Der-fon-Gor", 15),
                 new Person("Petr", "la-Petrov", 16),
+                new Person("Albert", "Van-Der-fon-Gor", 15),
                 new Person("Kir", "Der-fon-Persi", 15),
                 new Person("Ivan", "la-Ivanov", 15)
         ));
@@ -12,6 +12,6 @@ public class Main {
         for (Person person1 : person) {
             System.out.println(person1);
         }
-        //System.out.println(person);
+        System.out.println(person);
     }
 }
